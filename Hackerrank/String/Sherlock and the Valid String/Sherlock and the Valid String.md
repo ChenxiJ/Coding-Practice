906199efcfc9f76a69995e353d1c81f8948d8e4b
chenxi <chenxi.jia@fau.de>
2018-11-07 11:16:03 +0100

    valid

---
+++ Hackerrank/String/Sherlock and the Valid String/Sherlock and the Valid String.md
@@ -0,0 +1,8 @@
+Problem link: </br>
+https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem
+
+A string is *valid* if all characters of the string appear the same number of times. 
+It is also *valid* if can remove just 1 character at 1 index in the string, and the remaining characters
+will occur the same number of times. 
+
+Given a string *s* (all lower case characters), determine if it is *valid*. If so, return YES, otherwise return NO.
---
+++ Hackerrank/String/Sherlock and the Valid String/isValid.py
@@ -0,0 +1,27 @@
+def isValid(s):
+    mapp = {}
+    for i in s:
+        if i in mapp:
+            mapp[i] += 1
+        else:
+            mapp[i] = 1
+    count = mapp[s[0]]
+    diff = 0
+    for key, value in mapp.items():
+        if value == count:
+            continue
+        else:
+            if abs(count - value) > 1 and min(count, value) != 1:
+                return 'NO'
+            else:
+                # very important which case comes first, if write "if abs(count - value) == 1"...
+                # will be wrong. needs to see which case dominant!
+                if min(count, value) == 1:
+                    count = max(count, value)
+                    diff += 1
+                else:
+                    count = min(count, value)
+                    diff += 1
+    if diff > 1:
+        return 'NO'
+    return 'YES'
