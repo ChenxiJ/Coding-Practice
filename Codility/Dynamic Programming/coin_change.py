a3f94ef2d79d924a2a42831b7ef2ae230508cec4
Chenxi-JW <61411898+Chenxi-JW@users.noreply.github.com>
2020-03-22 23:25:32 +0000

    change name

--- Codility/Dynamic Programming/coin_change.py
+++ Codility/Dynamic Programming/coin_change.py
@@ -1,15 +1,15 @@
 import sys
 
 
-def min_coins(coin, value):
+def min_coins(coin, payment_value):
     # dp = [[0 for x in range(value + 1)] for x in range(len(coin) + 1)]
-    dp = [[0] * (value + 1) for x in range(len(coin) + 1)]
+    dp = [[0] * (payment_value + 1) for x in range(len(coin) + 1)]
     # rows are set of the i smallest valued coins, starting from 1
     # columns j are value to be paid, starting from 0 to value
 
-    dp[0] = [sys.maxsize] * (value + 1)
+    dp[0] = [sys.maxsize] * (payment_value + 1)
     for i in range(1, len(coin) + 1):
-        for j in range(1, value + 1):
+        for j in range(1, payment_value + 1):
             if j < coin[i - 1]:
                 # means previous coin already covered new value
                 dp[i][j] = dp[i - 1][j]
@@ -21,9 +21,9 @@ def min_coins(coin, value):
 
 # here can optimize space since only using previous row, no need to save all rows, only need 1-d array instead of matrix
 
-def min_coin_opt(coin, value):
+def min_coin_opt(coin, payment_value):
     dp = [0] + [sys.maxsize] * value
     for i in range(1, len(coin) + 1):
-        for j in range(coin[i - 1], value + 1):
+        for j in range(coin[i - 1], payment_value + 1):
             dp[j] = min(1 + dp[j - coin[i - 1]], dp[j])
     return dp[-1]
