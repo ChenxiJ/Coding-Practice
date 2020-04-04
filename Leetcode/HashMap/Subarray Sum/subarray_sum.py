842f3d7d3aafa5521ccb957e7b557937e936281a
Chenxi-JW <61411898+Chenxi-JW@users.noreply.github.com>
2020-04-04 14:47:11 +0100

    hash prefix sum

--- Leetcode/HashMap/Subarray Sum/subarray_sum.py
+++ Leetcode/HashMap/Subarray Sum/subarray_sum.py
@@ -1,3 +1,20 @@
+# this will time out, O(n^2)
+def subarray_sum1(nums, k):
+    n = len(nums)
+    dp = [[0] * n for _ in range(n)]
+    count = 0
+    for i in range(n):
+        for j in range(i, n):
+            if i == j:
+                dp[i][j] = nums[i]
+            else:
+                dp[i][j] = dp[i][j - 1] + nums[j]
+            if dp[i][j] == k:
+                count += 1
+    return count
+
+
+# use hash to store the count of prefix sum, O(n)
 def subarray_sum(nums, k):
     # initialize the dic so at the first element prefix is 0
     dic = {0: 1}
